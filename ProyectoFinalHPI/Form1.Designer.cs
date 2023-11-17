4d88c9ba307247f52856204accd3a89316320cb2
James <James@DESKTOP-TM0PTV0>
2023-11-17 06:59:40 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinalHPI/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ProyectoFinalHPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ldia = new System.Windows.Forms.Label();
+            this.Lhora = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Lpruebas = new System.Windows.Forms.Label();
+            this.Lpruebas2 = new System.Windows.Forms.Label();
+            this.AcelerarT = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 264);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Placa:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(592, 460);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Salida";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(511, 210);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(228, 244);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Forte", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(199, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(444, 35);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Parqueadero Fantasmatico 24H";
+            // 
+            // Ldia
+            // 
+            this.Ldia.AutoSize = true;
+            this.Ldia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ldia.Location = new System.Drawing.Point(6, 132);
+            this.Ldia.Name = "Ldia";
+            this.Ldia.Size = new System.Drawing.Size(0, 24);
+            this.Ldia.TabIndex = 7;
+            this.Ldia.Click += new System.EventHandler(this.Ldia_Click);
+            // 
+            // Lhora
+            // 
+            this.Lhora.AutoSize = true;
+            this.Lhora.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lhora.Location = new System.Drawing.Point(68, 165);
+            this.Lhora.Name = "Lhora";
+            this.Lhora.Size = new System.Drawing.Size(0, 24);
+            this.Lhora.TabIndex = 8;
+            this.Lhora.Click += new System.EventHandler(this.Lhora_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Hora:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(507, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Selecciona el carro a salir";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(666, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Valor Hora: 5000";
+            // 
+            // Lpruebas
+            // 
+            this.Lpruebas.AutoSize = true;
+            this.Lpruebas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lpruebas.Location = new System.Drawing.Point(849, 54);
+            this.Lpruebas.Name = "Lpruebas";
+            this.Lpruebas.Size = new System.Drawing.Size(80, 24);
+            this.Lpruebas.TabIndex = 12;
+            this.Lpruebas.Text = "Pruebas";
+            // 
+            // Lpruebas2
+            // 
+            this.Lpruebas2.AutoSize = true;
+            this.Lpruebas2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lpruebas2.Location = new System.Drawing.Point(858, 30);
+            this.Lpruebas2.Name = "Lpruebas2";
+            this.Lpruebas2.Size = new System.Drawing.Size(59, 24);
+            this.Lpruebas2.TabIndex = 13;
+            this.Lpruebas2.Text = "Timer";
+            this.Lpruebas2.Click += new System.EventHandler(this.Lpruebas2_Click);
+            // 
+            // AcelerarT
+            // 
+            this.AcelerarT.Location = new System.Drawing.Point(986, 522);
+            this.AcelerarT.Name = "AcelerarT";
+            this.AcelerarT.Size = new System.Drawing.Size(104, 23);
+            this.AcelerarT.TabIndex = 14;
+            this.AcelerarT.Text = "Siguiente Dia";
+            this.AcelerarT.UseVisualStyleBackColor = true;
+            this.AcelerarT.Click += new System.EventHandler(this.AcelerarT_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 522);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Parar Tiempo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 679);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.AcelerarT);
+            this.Controls.Add(this.Lpruebas2);
+            this.Controls.Add(this.Lpruebas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Lhora);
+            this.Controls.Add(this.Ldia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Ldia;
+        private System.Windows.Forms.Label Lhora;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Lpruebas;
+        private System.Windows.Forms.Label Lpruebas2;
+        private System.Windows.Forms.Button AcelerarT;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProyectoFinalHPI/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinalHPI
+{
+    public partial class Form1 : Form
+    {
+        //double t;
+        string RutaDocu = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string Dia;
+        DateTime Tiempo = DateTime.Now;
+        string[] CarrosParqueados = new string[20];
+        int[,] ValoresCarrosP = new int[20, 2];
+        int[,] CantidadDias = new int[20, 2];
+        int PosCarro = 0;
+        int indice;
+        int tiempo = 1000;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            RutaDocu += "\\pruebasParqueadero.txt";
+            textBox1.MaxLength = 6;
+
+            button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (!VerificacionPlaca())
+            {
+                MessageBox.Show("Placa Incorrecta");
+                textBox1.Text = "";
+
+            }
+            else if (VerificaionEspaciosYrepeticion() == 0)
+            {
+                if (PosCarro < CarrosParqueados.GetLength(0))
+                {
+                    //Lpruebas.Text = "";
+                    CarrosParqueados[PosCarro] = textBox1.Text;
+                    // Tiempos[PosCarro, 0] = Tiempo.Hour;
+                    ///CarrosParqueados[PosCarro, 1] = $"{Dia} {Tiempo.Hour}:{Tiempo.Minute}";
+                    if (Tiempo.Hour == 0) {
+                        ValoresCarrosP[PosCarro, 0] = 60 * 12 + Tiempo.Minute;
+                        //ValoresCarrosP[PosCarro, 0] +=Tiempo.Minute;
+
+                    }
+                    else {
+                        ValoresCarrosP[PosCarro, 0] = 60 * Tiempo.Hour + Tiempo.Minute;
+                        //ValoresCarrosP[PosCarro, 0] +=Tiempo.Minute;
+                    }
+                    checkedListBox1.Items.Add(textBox1.Text);
+                    CantidadDias[PosCarro, 0] = Tiempo.Day;
+                    //Tiempo.Day;
+                    StreamWriter W2 = new StreamWriter(RutaDocu, true);
+                    W2.WriteLine($"{Dia} {Tiempo.Hour}:{Tiempo.Minute}");
+                    W2.Close();
+                    StreamWriter W1 = new StreamWriter(RutaDocu, true);
+                    W1.WriteLine(textBox1.Text);
+                    W1.Close();
+                    StreamWriter W3 = new StreamWriter(RutaDocu, true);
+                    W3.WriteLine(" ");
+                    W3.Close();
+                    StreamWriter W4 = new StreamWriter(RutaDocu, true);
+                    W4.WriteLine(" ");
+                    W4.Close();
+                    textBox1.Text = "";
+                    //PosCarro++;
+                }
+                else { button1.Enabled = false; }
+
+
+
+                //W3.Close();
+                //string[] Arreglito = File.ReadAllLines(RutaDocu);
+
+
+            }
+            else
+            {
+                MessageBox.Show("Placa Duplicada o Parqueadero sin espacio");
+                textBox1.Text = "";
+            }
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            indice = checkedListBox1.SelectedIndex;
+            ImprimirSalidatxt(checkedListBox1.Items[indice].ToString());
+            // Lpruebas.Text = checkedListBox1.Items[indice].ToString();
+
+            checkedListBox1.Items.Remove(checkedListBox1.Items[indice]);
+            //CarrosParqueados[indice, 2] = $"{Dia}  {Tiempo.Hour}:{Tiempo.Minute}";
+
+
+            //StreamWriter W1 = new StreamWriter(RutaDocu, true);
+            //W1.WriteLine($"{Dia}  {Tiempo.Hour}:{Tiempo.Minute}");
+            //W1.Close();
+            //label1.Text = indice.ToString();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            indice = checkedListBox1.SelectedIndex;
+            if (indice == -1)
+            {
+                button2.Enabled = false;
+            }
+            else { button2.Enabled = true; }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool VerificacionPlaca() {
+            textBox1.Text += "      ";
+            for (int i = 0; i <= 2; i++)
+            {
+
+                if (!char.IsLetter(textBox1.Text[i]))
+                {
+                    return false;
+
+                }
+
+                if (!char.IsNumber(textBox1.Text[3 + i]))
+                {
+                    return false;
+
+
+                }
+
+            }
+            return true;
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Lpruebas.Text = timer1.Interval.ToString();
+
+            Tiempo = Tiempo.AddHours(0.25);
+            Lhora.Text = Tiempo.Hour.ToString();
+            Lhora.Text += ": " + Tiempo.Minute.ToString();
+            Dia = Tiempo.DayOfWeek.ToString();
+            Ldia.Text = Dia;
+
+        }
+
+        private void Ldia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lhora_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ImprimirSalidatxt(string CarroSalir) {
+            int PosicionCarro = 0;
+            double TotalPagar = 0;
+            string[] Arreglito = File.ReadAllLines(RutaDocu);
+
+            for (int i = 0; i < CarrosParqueados.Length; i++)
+            {
+
+                if (CarrosParqueados[i] == CarroSalir)
+                {
+                    CarrosParqueados[i] = null;
+                    PosicionCarro = i;
+
+                }
+
+            }
+
+            CantidadDias[PosicionCarro, 1] = Tiempo.Day;
+            if (CantidadDias[PosicionCarro, 0] - CantidadDias[PosicionCarro, 1] > 1){
+            
+            TotalPagar += 1440*((CantidadDias[PosicionCarro, 0] - CantidadDias[PosicionCarro, 1])-1);
+            
+            }
+
+
+
+            ValoresCarrosP[PosicionCarro, 1] = 60 * Tiempo.Hour + Tiempo.Minute;
+            if (ValoresCarrosP[PosicionCarro, 1] < ValoresCarrosP[PosicionCarro, 0])
+            {
+                TotalPagar = 24 * 60 - ValoresCarrosP[PosicionCarro, 0];
+                TotalPagar += ValoresCarrosP[PosicionCarro, 1];
+                TotalPagar = TotalPagar * 83.3333334;
+
+
+
+            }
+            else
+            {
+                TotalPagar = ValoresCarrosP[PosicionCarro, 1] - ValoresCarrosP[PosicionCarro, 0];
+                TotalPagar *= 83.3333334;
+
+
+            }
+           
+
+            //ValoresCarrosP[PosCarro, 1] += ;
+            for (int i = 0; i < Arreglito.Length; i++) {
+
+                if (Arreglito[i] == CarroSalir) {
+
+                    Arreglito[i+1] = $"{Dia}  {Tiempo.Hour}:{Tiempo.Minute}";
+                    Arreglito[i + 2] = $"Total Pagado: {TotalPagar}";
+                
+
+
+                }
+            }
+
+            
+                File.WriteAllLines(RutaDocu, Arreglito);
+
+
+        }
+
+        public int VerificaionEspaciosYrepeticion() {
+            int ban = 0;
+            for (int i = 0; i < CarrosParqueados.Length; i++) {
+                //Lpruebas2.Text = i.ToString();
+
+                if (CarrosParqueados[i] == null)
+                {
+                    // ban++;
+                   Lpruebas.Text = "bueno"+i;
+                    PosCarro = i;
+                    ban = 0;
+                    break;
+                   
+
+                }
+                else { ban++; }
+            }
+            for (int o = 0; o < CarrosParqueados.Length; o++)
+            {
+                if (CarrosParqueados[o] == textBox1.Text)
+                {
+                    ban++;
+                    return ban;
+
+                }
+            }
+            return ban;
+        
+        
+        }
+
+        private void AcelerarT_Click(object sender, EventArgs e)
+        {
+          Tiempo= Tiempo.AddDays(1);
+          Dia = Tiempo.DayOfWeek.ToString(); 
+          Ldia.Text = Dia;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+
+            }
+            else { timer1.Enabled = true; }
+        }
+
+        private void Lpruebas2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
